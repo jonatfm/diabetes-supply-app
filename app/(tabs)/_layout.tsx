6c9c5117b14afe273395b3ec7d9d799395413a51
Jonathan Fortmann <jonathan.fortmann@gmail.com>
2026-01-19 21:05:47 +0100

    Added holiday tab for navigation and added date range picker

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,27 +1,34 @@
-import { withLayoutContext } from 'expo-router';
-import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import { withLayoutContext } from "expo-router";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 
 const { Navigator } = createMaterialBottomTabNavigator();
 
 export const MaterialBottomTabs = withLayoutContext(Navigator);
 
 export default function TabLayout() {
-  return (
-    <MaterialBottomTabs>
-      <MaterialBottomTabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: 'home',
-        }}
-      />
-      <MaterialBottomTabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: 'cog',
-        }}
-      />
-    </MaterialBottomTabs>
-  );
+    return (
+        <MaterialBottomTabs>
+            <MaterialBottomTabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: "home",
+                }}
+            />
+            <MaterialBottomTabs.Screen
+                name="holidayScreen"
+                options={{
+                    title: "Plan Holiday",
+                    tabBarIcon: "beach",
+                }}
+            />
+            <MaterialBottomTabs.Screen
+                name="settings"
+                options={{
+                    title: "Settings",
+                    tabBarIcon: "cog",
+                }}
+            />
+        </MaterialBottomTabs>
+    );
 }
--- app/(tabs)/holidayScreen.tsx
+++ app/(tabs)/holidayScreen.tsx
@@ -1,44 +1,81 @@
 import AppWrapper from "@/components/AppWrapper";
-import NumberInput from "@/components/NumberInput";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ScrollView, View } from "react-native";
-import { Card, Text, TextInput } from "react-native-paper";
+import { Button, Card, Portal, Text, TextInput } from "react-native-paper";
+import { DatePickerModal } from "react-native-paper-dates";
+import { RangeChange } from "react-native-paper-dates/lib/typescript/Date/Calendar";
 
 export default function HolidayScreen() {
     const [location, setLocation] = useState<string>("");
-    const [numberOfDays, setNumberOfDays] = useState<number|null>(null);
+    const [numberOfDays, setNumberOfDays] = useState<number | null>(null);
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState<boolean>(false);
+    const [startDate, setStartDate] = useState<Date | undefined>(undefined);
+    const [endDate, setEndDate] = useState<Date | undefined>(undefined);
+    
+
+    const onConfirm = useCallback<RangeChange>(({startDate, endDate}) => {
+        setIsDatePickerOpen(false);
+        setStartDate(startDate);
+        setEndDate(endDate);
+        console.log("[on-change-multi]", {startDate, endDate});
+    }, []);
 
     return (
         <AppWrapper>
             <ScrollView
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={{paddingBottom: 24}}
+                contentContainerStyle={{ paddingBottom: 24 }}
             >
-                <Text variant="headlineLarge" style={{marginBottom: 24}}>Plan a Holiday</Text>
+                <Text variant="headlineLarge" style={{ marginBottom: 24 }}>
+                    Plan a Holiday
+                </Text>
 
-                <Text variant="titleLarge" style={{marginBottom: 12}}>Start new</Text>
-                <Card elevation={1} style={{marginBottom: 24}}>
-                    <Card.Content style={{gap: 12}}>
+                <Text variant="titleLarge" style={{ marginBottom: 12 }}>
+                    Plan new holiday
+                </Text>
+                <Card elevation={1} style={{ marginBottom: 24 }}>
+                    <Card.Content style={{ gap: 12 }}>
                         <View>
-                            <Text variant="titleMedium">Where are you going?</Text>
+                            <Text variant="titleMedium">
+                                Where are you going?
+                            </Text>
                             <TextInput
                                 label="Location"
                                 value={location}
-                                onChangeText={text => setLocation(text)}
+                                onChangeText={(text) => setLocation(text)}
                             />
                         </View>
                         <View>
-                            <Text variant="titleMedium">How long are you staying?</Text>
-                            <NumberInput
-                                label="Number of days"
-                                value={numberOfDays}
-                                onChangeText={text => setNumberOfDays(text)}
-                                minValue={1}
-                            />
+                            <Text variant="titleMedium">
+                                When will you be away?
+                            </Text>
+                            <Button
+                                mode={(startDate && endDate) ? "outlined" : "contained"}
+                                onPress={() => setIsDatePickerOpen(true)}
+                            >
+                                Select Dates
+                            </Button>
+                            {(startDate && endDate) && (
+                                <Text>
+                                    From {startDate.toDateString()} to {endDate.toDateString()}
+                                </Text>
+                            )}
                         </View>
                     </Card.Content>
                 </Card>
             </ScrollView>
+
+            <Portal>
+                <DatePickerModal
+                    locale="en"
+                    mode="range"
+                    visible={isDatePickerOpen}
+                    onDismiss={() => setIsDatePickerOpen(false)}
+                    startDate={startDate}
+                    endDate={endDate}
+                    onConfirm={onConfirm}
+                />
+            </Portal>
         </AppWrapper>
-    )
+    );
 }
